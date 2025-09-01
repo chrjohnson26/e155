c3b76dcdd6802f629f5958ecb97e02b2a7ca3080
Christian Johnson <chrjohnson@hmc.edu>
2025-09-01 13:56:53 -0700

    adding project files

--- lab_1/demos/fpga_dev_board_test/source/impl_1/top.sv
+++ lab_1/demos/fpga_dev_board_test/source/impl_1/top.sv
@@ -1,23 +1,22 @@
 module top(
-	input 	logic	mcu_blink_in,
-	output 	logic fpga_blink_out, mcu_echo_led
+     input   logic reset,
+     output  logic led
 );
 
-	logic int_osc;
-	logic pulse;
-	logic led_state = 0;
-	logic [24:0] counter = 0;
-	
-	// Internal high-speed oscillator
-	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-	
-	// Simple clock divider
-	always_ff @(posedge int_osc)
-		begin
-			counter <= counter + 1;
-		end
-		
-  assign fpga_blink_out = counter[24];
-	assign mcu_echo_led = mcu_blink_in;
-
+   logic int_osc;
+   logic [24:0] counter;
+  
+   // Internal high-speed oscillator
+   HSOSC #(.CLKHF_DIV(2'b01)) 
+         hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+  
+   // Counter
+   always_ff @(posedge int_osc) begin
+     if(reset == 0)  counter <= 0;
+     else            counter <= counter + 1;
+   end
+  
+   // Assign LED output
+   assign led = counter[24];
+  
 endmodule
