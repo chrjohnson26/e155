68ac9377ab203cf9a090edcc77c23d64213a8a1b
Christian Johnson <chrjohnson@hmc.edu>
2025-09-02 18:59:02 -0700

    updating led_controller

--- lab_1/fpga/src/led_controller.sv
+++ lab_1/fpga/src/led_controller.sv
@@ -7,12 +7,16 @@
     System Verilog practice interfacing with three LEDs on the dev board 
 */
 
+/*
+	led_controller controls three LEDs based on four switch inputs
+*/
 module led_controller(input logic [3:0] s,
             output logic [2:0] led);
     
 
     logic int_osc;
     logic [25:0] counter;
+	logic led2_reg = 1'b0;
 
 
     // To generate the clock use the onboard high-speed oscillator
@@ -26,15 +30,16 @@ module led_controller(input logic [3:0] s,
 
     // led[2]
     // hf_osc is oscillating at 48MHz, we want led[2] to blink at 2.4Hz
-    // So we want the MSB to turn on every 48MHz / 2.4 Hz = 20 MHz
+    // So we want the MSB to turn on every 24MHz / 2.4 Hz / 2 = 5 MHz
     always_ff @(posedge int_osc) begin
-        // if (counter == 25'd20000000) begin
-            // led[2] <= ~led[2];
-        //     counter <= 0;
-        // end
-        counter <= counter + 1'b1;
+         if (counter == 25'd5000000) begin
+             led2_reg <= ~led2_reg;
+             counter <= 0;
+        end else begin
+			counter <= counter + 1'b1;
+		end
     end
 
-    assign led[2] = (counter == 25'd2000000);
+    assign led[2] = led2_reg;
 
 endmodule
